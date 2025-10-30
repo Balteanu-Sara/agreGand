2592b819e28ac3b3888b131241252449b1af2caa
sarabalteanu <sara.balteanu.03@gmail.com>
2025-10-30 19:16:38 +0200

    Changed styling of App component + Added Router to link the 'News' and 'Home' pages

--- agreGând/src/App.jsx
+++ agreGând/src/App.jsx
@@ -1,40 +1,15 @@
-import "./App.css";
-import { useData } from "./services/useData.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import News from "./pages/News";
 
 function App() {
-  const { articles, loading, error } = useData();
-
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error fetching data: {error.message}</div>;
-
   return (
-    <>
-      <h1>agreGând</h1>
-      <ul>
-        Stiri de actualitate:
-        {articles.map((article) => {
-          return (
-            <li key={article.title + article.link}>
-              <h2>
-                <a href={article.link} target="_blank">
-                  {article.title}
-                </a>
-              </h2>
-              <br />
-              <p>Descriere: {article.description}</p>
-              {article.image && <img src={article.image} alt={article.title} />}
-              <p>Sursa: {article.source}</p>
-              <p>Data publicarii: {article.publishDate}</p>
-              {article.categories.length > 0 &&
-                article.categories.map((cat, index) => (
-                  <span key={cat + index}>| {cat} | </span>
-                ))}
-              <hr />
-            </li>
-          );
-        })}
-      </ul>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/news" element={<News />} />
+      </Routes>
+    </Router>
   );
 }
 
