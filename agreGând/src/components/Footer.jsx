803d88fd5261879359096d03e188e487da48c6ac
sarabalteanu <sara.balteanu.03@gmail.com>
2025-11-03 23:25:32 +0200

    Base of common 'Footer' component

--- agreGând/src/components/Footer.jsx
+++ agreGând/src/components/Footer.jsx
@@ -1,12 +1,39 @@
 import { Facebook, Twitter, Instagram } from "lucide-react";
+import { useState } from "react";
 
 export default function Footer() {
+  const [query, setQuery] = useState("");
+  const [message, setMessage] = useState("");
+
+  function handleClick() {
+    if (query.length === 0) {
+      setMessage("Adresa de email nu poate fi goală.");
+      return;
+    }
+    if (!/\S+@\S+\.\S+/.test(query)) {
+      setMessage("Adresa de email nu este validă.");
+      return;
+    }
+    setMessage("Te-ai abonat cu succes!");
+    setQuery("");
+  }
+
   return (
     <div className="footer">
       <h1>Abonează-te la noutățile noastre</h1>
-      <label htmlFor="subscribe">Introdu adresa ta de email:</label>
-      <input type="text" name="subscribe" />
-      <button>Abonează-te</button>
+      <div className="user-input">
+        <label htmlFor="subscribe">Introdu adresa ta de email:</label>
+        <input
+          type="text"
+          name="subscribe"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button onClick={handleClick}>
+          <span>Abonează-te</span>
+        </button>
+        {message && <p>{message}</p>}
+      </div>
       <div className="logos">
         <Facebook />
         <Twitter />
