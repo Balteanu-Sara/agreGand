b123686535c63caffadbe27e92572843926ee020
sarabalteanu <sara.balteanu.03@gmail.com>
2025-11-17 13:01:51 +0200

    Adapted footer for bigger screen dimensions

--- agreGând/src/components/Footer.jsx
+++ agreGând/src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import { Facebook, Twitter, Instagram } from "lucide-react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   const [query, setQuery] = useState("");
   const [message, setMessage] = useState("");
+  const currentUrl = window.location.href;
 
   function handleClick() {
     if (query.length === 0) {
@@ -20,32 +22,83 @@ export default function Footer() {
 
   return (
     <div className="footer">
-      <h1>Abonează-te la noutățile noastre!</h1>
-      <div className="user-input">
-        <label htmlFor="subscribe">Introdu adresa ta de email:</label>
-        <div>
-          <input
-            type="text"
-            name="subscribe"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          />
+      <div className="sides">
+        <div className="left-side">
+          <h1>Abonează-te la noutățile noastre!</h1>
+          <div className="user-input">
+            <label htmlFor="subscribe">Introdu adresa ta de email:</label>
+            <div>
+              <input
+                type="text"
+                name="subscribe"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+            </div>
+            <button onClick={handleClick}>
+              <span>Abonează-te</span>
+            </button>
+            {message && <p>{message}</p>}
+          </div>
+          <div className="logos">
+            <a href="">
+              <Facebook />
+            </a>
+            <a href="">
+              <Twitter />
+            </a>
+            <a href="">
+              <Instagram />
+            </a>
+          </div>
         </div>
-        <button onClick={handleClick}>
-          <span>Abonează-te</span>
-        </button>
-        {message && <p>{message}</p>}
-      </div>
-      <div className="logos">
-        <a href="">
-          <Facebook />
-        </a>
-        <a href="">
-          <Twitter />
-        </a>
-        <a href="">
-          <Instagram />
-        </a>
+        <div className="right-side">
+          <ul>
+            <li>
+              <Link
+                to="/"
+                className={
+                  "menu-element" + (currentUrl.endsWith("/") ? " selected" : "")
+                }
+              >
+                Acasă
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/news"
+                className={
+                  "menu-element" +
+                  (currentUrl.includes("news") ? " selected" : "")
+                }
+              >
+                Știri
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/resources"
+                className={
+                  "menu-element" +
+                  (currentUrl.includes("resources") ? " selected" : "")
+                }
+              >
+                Resurse
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/about-us"
+                className={
+                  "menu-element" +
+                  (currentUrl.includes("about-us") ? " selected" : "")
+                }
+              >
+                Despre noi
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
       <p>
         Copyright © {new Date().getFullYear()} <strong>agreGând</strong> -
